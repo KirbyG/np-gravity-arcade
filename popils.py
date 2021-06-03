4b666263c20c15f6f6502b2b107e31e1e5912468
Kirby Gordon <kirby@Kirbys-MacBook-Pro.local>
2021-06-02 20:47:51 -0400

    outline for parsing user input and add some TODOs

--- popils.py
+++ popils.py
@@ -1,12 +1,13 @@
+#TODO refactor kirby's bad code
 import sys
-sys.path.append('/Users/kirby/anaconda3/lib/python3.7/site-packages/')
+#sys.path.append('/Users/kirby/anaconda3/lib/python3.7/site-packages/')
 import pygame
 import random
 pygame.init()
 size = [801, 601]
 ROW = 1
 COL = 0
-BLOCK = 50
+BLOCK = 50 #block size = 50px TODO dynamic block sizing
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Test')
 player = (255, 0, 0) #red
@@ -49,18 +50,47 @@ state[rows - 4][13] = hard
 state[rows - 4][12] = hard
 state[rows - 4][11] = ladder
 state[rows - 4][10] = ladder
+
+
+def place_gadget(variable_states, coords): #TODO 3sat to game state conversion function
+    pass
 for row in range(rows):
     state[row][0] = hard
     state[row][cols - 1] = hard
+#set player position
 redRow = rows - 2
 redCol = 1
 hidden = support
+#TODO multiple 3sat instances stored in files
+#get 3sat input or use default
+three_sat = input('enter 3sat instance in the format: -3 -2 4 2 7 -4, or enter a filename:')
+test_input = '!3 !2 4 2 7 !4'
+print(str.split(test_input))
+
+test_input = '-3 -2 4 2 7 -4'
+array_form = str.split(test_input)
+from enum import Enum
+class state (Enum):
+    UNUSED = 0
+    NEGATED = -1
+    NONNEGATED = 1
+coords = [0, 0]
+for i in range(len(array_form) / 3):
+    index = 3 * i
+    variable_states = [state.UNUSED for i in range(max(abs(array_form)))]
+    for j in range(3):
+        temp = array_form[i + j]
+        variable_states[abs(temp) - 1] = abs(temp) / temp
+    place_gadget(variable_states, coords)
+    coords[1] = coords[1] + 6
+    
+    
 ###setup
-while (done == False):
+while (done == False): #TODO block breaking logic
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-        elif event.type == pygame.KEYUP:
+        elif event.type == pygame.KEYUP: #TODO functions!
             if event.key == pygame.K_LEFT and state[redRow][redCol - 1] != hard:
                 state[redRow][redCol] = hidden
                 redCol = redCol - 1
@@ -90,7 +120,7 @@ while (done == False):
     #render
     for row in range(rows):
         for col in  range(cols):
-            pygame.draw.rect(screen, state[row][col], [col * BLOCK + 1, row * BLOCK + 1, 49, 49])
+            pygame.draw.rect(screen, state[row][col], [col * BLOCK + 1, row * BLOCK + 1, BLOCK - 1, BLOCK - 1])
     #render
     pygame.display.flip()
     clock.tick(20);
