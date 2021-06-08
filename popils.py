e034b88a1998c60e4b4863dd62d9ba5527c297d3
Kirby Gordon <kirbyjgordon@gmail.com>
2021-06-07 20:59:00 -0400

    fix visual glitch

--- popils.py
+++ popils.py
@@ -60,9 +60,11 @@ hidden = SUPPORT
 
 # TODO multiple 3sat instances stored in files
 array_form = [int(el) for el in str.split(three_sat)]
+tuples = int(len(array_form) / 3)
+nested_form = [[array_form[3 * i + j] for j in range(3)] for i in range(tuples)]
 
 row_pointer = 3
-tuples = int(len(array_form) / 3)
+
 num_vars = max([abs(el) for el in array_form])
 # setup
 rows = 6 * (tuples + 1)
@@ -84,59 +86,128 @@ state[2][cols - 2] = LADDER
 state[rows - 2][cols - 2] = PRINCESS
 state[rows - 3][cols - 2] = SOFT
 # gadgets
-print(array_form)
+def sign(num):
+    return int(abs(num) / num)
 for i in range(tuples):
-    index = 3 * i
     variable_states = [UNUSED for k in range(num_vars)]
     for j in range(3):
-        temp = array_form[index + j]
-        variable_states[abs(temp) - 1] = int(abs(temp) / temp)
+        temp = nested_form[i][j]
+        variable_states[abs(temp) - 1] = sign(temp)
     place_gadget(variable_states, row_pointer)
     row_pointer = row_pointer + 6
 
 # setup
+screen.fill(BACKGROUND)
+def draw(min_row, max_row, min_col, max_col):
+    global state, screen
+    for row in range(min_row, max_row + 1):
+        for col in range(min_col, max_col + 1):
+            pygame.draw.rect(screen, state[row][col],
+                             [col * BLOCK + 1, (rows - row - 1) * BLOCK + 1, BLOCK - 1, BLOCK - 1])
+    pygame.display.update()
+draw(0, rows - 1, 0, cols - 1)
+def move(vector):
+    global state, hidden, redRow, redCol, fresh
+    
+    if vector == UP and state[redRow + 1][redCol] == SOFT:
+        fresh = False
+        
+    else:
+        state[redRow][redCol] = hidden
+        redRow = redRow + vector[0]
+        redCol = redCol + vector[1]
+        hidden = state[redRow][redCol]
+        state[redRow][redCol] = PLAYER
+        draw(min(redRow, redRow - vector[0]), max(redRow, redRow - vector[0]), 
+             min(redCol, redCol - vector[1]), max(redCol, redCol - vector[1]))
+def force(vector):
+    global state, hidden, redRow, redCol, fresh
+    target = state[redRow + vector[0]][redCol + vector[1]]
+    if vector == UP:
+        if target == SOFT:
+            fresh = False
+            for falling_row in range(redRow + 1, rows - 1):
+                state[falling_row][redCol] = state[falling_row + 1][redCol]
+            state[rows - 1][redCol] = HARD
+            draw(redRow + 1, rows - 1, redCol, redCol)
+        elif hidden == LADDER and (target != HARD):
+            move(UP)
+    elif target != HARD:
+        move(vector)
 done = False
-while done is False:  # TODO block breaking logic
+fresh = True
+solution = []
+autosolve = -1
+count = 0
+def passes(var, guess):
+    return sign(var) * guess[abs(var) - 1] == 1
+while done is False:
+    if autosolve > -1: #TODO why is this slow??
+        force(solution[autosolve])
+        autosolve = autosolve + 1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-        elif event.type == pygame.KEYUP:  # TODO functions!
-            if event.key == pygame.K_LEFT and state[redRow][redCol - 1] != HARD:
-                state[redRow][redCol] = hidden
-                redCol = redCol - 1
-                hidden = state[redRow][redCol]
-                state[redRow][redCol] = PLAYER
-            elif event.key == pygame.K_RIGHT and state[redRow][redCol + 1] != HARD:
-                state[redRow][redCol] = hidden
-                redCol = redCol + 1
-                hidden = state[redRow][redCol]
-                state[redRow][redCol] = PLAYER
-            elif event.key == pygame.K_UP:
-                if state[redRow + 1][redCol] == SUPPORT or state[redRow + 1][redCol] == LADDER and hidden == LADDER:
-                    state[redRow][redCol] = hidden
-                    redRow = redRow + 1
-                    hidden = state[redRow][redCol]
-                    state[redRow][redCol] = PLAYER
-                elif state[redRow + 1][redCol] == SOFT:
-                    for falling_row in range(redRow + 1, rows - 1):
-                        state[falling_row][redCol] = state[falling_row + 1][redCol]
-                    state[rows - 1][redCol] = HARD
-            elif event.key == pygame.K_DOWN and state[redRow - 1][redCol] != HARD:
-                state[redRow][redCol] = hidden
-                redRow = redRow - 1
-                hidden = state[redRow][redCol]
-                state[redRow][redCol] = PLAYER
+        else:
+            if event.type == pygame.KEYUP:  # TODO functions!
+                if event.key == pygame.K_LEFT:
+                    force(LEFT)
+                elif event.key == pygame.K_RIGHT:
+                    force(RIGHT)
+                elif event.key == pygame.K_UP:
+                    force(UP)
+                elif event.key == pygame.K_DOWN and state[redRow - 1][redCol] != HARD:
+                    force(DOWN)
         while (state[redRow - 1][redCol] == SUPPORT and hidden == SUPPORT):
-            state[redRow][redCol] = hidden
-            redRow = redRow - 1
-            hidden = state[redRow][redCol]
-            state[redRow][redCol] = PLAYER
-    screen.fill(BACKGROUND)
-    # render
-    for row in range(rows):
-        for col in range(cols):
-            pygame.draw.rect(screen, state[rows - row - 1][col],
-                             [col * BLOCK + 1, row * BLOCK + 1, BLOCK - 1, BLOCK - 1])
-    pygame.display.flip()
+            force(DOWN)
+    if fresh and redRow == 1 and redCol == 1 and event.type == pygame.KEYUP and event.key == pygame.K_a:
+        #autosolve
+        autosolve = 0
+        #compute vector sequence
+        #find the solving variable assignment by brute force
+        good_guess = []
+        for guess in range(2 ** num_vars):
+            form = r'{:0' + str(num_vars) + r'b}'
+            parsed_guess = [int(form.format(guess)[j]) * 2 - 1 for j in range(num_vars)]
+            solve = True
+            for clause in range(tuples):
+                satisfied = False
+                for var in range(3):
+                    temp = nested_form[clause][var]
+                    if passes(temp, parsed_guess):
+                        satisfied = True
+                        parsed_guess
+                if satisfied is False:
+                    solve = False
+                    break
+            if solve:
+                good_guess = parsed_guess
+        if len(good_guess) == 0:
+            done = True
+        else:
+            #set variables
+            for var in good_guess:
+                if var == 1:
+                    solution.append(UP)
+                solution.append(RIGHT)
+                solution.append(RIGHT)
+            #traverse level
+            for tuple in range(tuples):
+                solution.append(UP)
+                solution.append(UP)
+                solution.append(UP)
+                shuffle = 2 * (11 - max([abs(nested_form[tuple][var]) for var in range(3) if passes(nested_form[tuple][var], good_guess)]))
+                for i in range(shuffle):
+                    solution.append(LEFT)
+                solution.append(UP)
+                solution.append(UP)
+                for i in range(shuffle):
+                    solution.append(RIGHT)
+                solution.append(UP)
+            solution.append(UP)
+            solution.append(UP)
+            solution.append(UP)
+    print('loop', count)
+    count = count + 1
     clock.tick(20)
 pygame.quit()
